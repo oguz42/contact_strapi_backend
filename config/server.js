e4851dfe46ef8981746c34bb736816e3d68a405d
oguz42 <oguzsevin67@gmail.com>
2021-05-22 17:40:05 +0300

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3730c7dca0833e3091ec40ce942d558b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c65939d6-398b-4b2f-91a9-2b8adbce769f'
+};
